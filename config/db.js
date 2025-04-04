7a1adf4f9cc032dda12f7241b499aaa3d84170a0
yvmeli <yameli1715@gmail.com>
2025-04-03 20:55:12 -0400

    Updated

--- config/db.js
+++ config/db.js
@@ -1,5 +1,3 @@
-const mongoose = require('mongoose');
-<<<<<<< Updated upstream
 const colors = require('colors');
 
 const connectDB = async () => {
@@ -16,19 +14,3 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
-
-=======
-require('dotenv').config();
-
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/crud-api');
-    console.log(`MongoDB Connected: ${conn.connection.host}`.blue.underline);
-  } catch (error) {
-    console.error('Error:', error.message);
-    process.exit(1);
-  }
-};
-
->>>>>>> Stashed changes
-module.exports = connectDB;
